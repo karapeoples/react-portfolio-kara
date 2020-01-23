4b8a28f937becad6d0f4ac7c3b6c676e73e56f7c
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-01-23 06:02:52 -0600

    :red_circle: Router Added, Links and Components Started :red_circle: Because: --{Good Start and Stopping Point} --{Checked and Made sure Routes and Things were working} --{Included Images}
    This commit:
    ~~First Actual Commit beyond BoilerPlate

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import avatar from '../images/avatar.jpg';
+
+const About = () => {
+
+  return(
+    <div>
+      <div>
+        <img class="avatar" src={avatar} alt='Kara R. Peoples' />
+      </div>
+      <h1>Kara R. Peoples</h1>
+      <p>Door the scarcely the that quaff oer wished not. Is still rare divining seat suddenly a thy a, leave whom the fact raven all the raven the thy. Still lore hear and this bends had, and thy thrilled nevermore i nevermore or above he and. At floor and being on and upon friends i, take books door spoken a the of than tossed. Ever off my grew there silken that more, this door sitting hath.</p>
+    </div>
+  )
+}
+
+export default About;
