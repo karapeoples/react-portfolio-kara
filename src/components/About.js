4d9ebb9048f5d4557e6e71f109e3c63f02c56128
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-01-23 08:15:08 -0600

    :art: About Section and Skills Section :art:
    Because:
    --{Content all Added to Both Sections}
    --{Images added to both Sections}
    --{Both Sections Ready to be Styled}
    
    This commit:
    ~~Pushes Up Content for Both About and Skills Section
    ~~Also the Nav is Completed with Pre-Styling More Styling Needed

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import avatar from '../images/avatar.jpg';
 
+
+let oSkills = ['Fast Learner', 'Good Attitude', 'Friendly', 'Hard-Worker', 'Honest', "Creative"]
+
 const About = () => {
 
   return(
@@ -9,7 +12,14 @@ const About = () => {
         <img class="avatar" src={avatar} alt='Kara R. Peoples' />
       </div>
       <h1>Kara R. Peoples</h1>
-      <p>Door the scarcely the that quaff oer wished not. Is still rare divining seat suddenly a thy a, leave whom the fact raven all the raven the thy. Still lore hear and this bends had, and thy thrilled nevermore i nevermore or above he and. At floor and being on and upon friends i, take books door spoken a the of than tossed. Ever off my grew there silken that more, this door sitting hath.</p>
+      <ul>
+        {oSkills.map(info => {
+         return(
+           <li> {info} </li>
+        )})}
+      </ul>
+     
+      <p>Being Disabled has taught me to appreciate the social experience that a job brings, while being thankful for being able to be a productive member of society. It has also allowed me the opportunity to craft and hone my programming skills. Crafting applies to my programming because it has taught a distinct attention for detail allowing me to pinpoint syntax errors with some comfort and patience. It also helps in allowing parts of a whole too make a complete picture in my mind. It has taught me both how to follow directions and make my own path. Honing my programming skills, with an education at Lambda School which provides much guided learning and project apps that could become marketable someday. Please checkout my GitHub Link to see all my progress and projects</p>
     </div>
   )
 }
