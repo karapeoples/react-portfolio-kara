3a0332d8a6864def279b83a21333f0c897349209
karapeoples <52808542+karapeoples@users.noreply.github.com>
2020-05-23 06:46:05 -0500

    Updates 5-23-2020

--- src/App.js
+++ src/App.js
@@ -4,60 +4,46 @@ import "./App.css";
 import Landing from "./components/Landing";
 import About from "./components/About";
 import logo from "./images/logo.png";
-import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import GitHub from "./components/git-hub/GitHub";
 import Showcase from "./components/Showcase";
-import { Button } from "reactstrap";
+import { bubble as Menu } from 'react-burger-menu'
 
-const navButton = {
-	background: "gold",
-	color: "maroon",
-	border: "2px solid maroon",
-	fontSize: "18px",
-	fontFamily: "Forum, cursive",
-};
 
 function App() {
+
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<Link to="/">
-					<img src={logo} alt="lambda-grad" className="logo" />
+		<div className='App'>
+			<header className='App-header'>
+				<Link to='/'>
+					<img src={logo} alt='lambda-grad' className='logo' />
 				</Link>
 
-				<nav>
-					<Link to="/showcase" className="link">
-						<Button style={navButton}>Showcase</Button>
+				<Menu id='hambrgbtn' right>
+					<Link to='/showcase' className='menu-item'>
+						Showcase
 					</Link>
-
-					<Link to="/about" className="link">
-						<Button style={navButton}>About</Button>
+					<Link to='/about' className='menu-item'>
+						About
 					</Link>
-
-					<Link to="/skills" className="link">
-						<Button style={navButton}>Skills</Button>
+					<Link to='/git-hub' className='menu-item'>
+						GitHub
 					</Link>
-
-					<Link to="/git-hub" className="link">
-						<Button style={navButton}>GitHub</Button>
+					<Link className='menu-item' to='/contact'>
+						Contact
 					</Link>
-
-					<Link to="/contact" className="link">
-						<Button style={navButton}>Contact</Button>
-					</Link>
-				</nav>
+				</Menu>
 			</header>
 			<div>
-				<Route exact path="/" component={Landing} />
-				<Route path="/about" component={About} />
-				<Route path="/skills" component={Skills} />
-				<Route path="/git-hub" component={GitHub} />
-				<Route path="/contact" component={Contact} />
-				<Route path="/showcase" component={Showcase} />
+				<Route exact path='/' component={Landing} />
+				<Route path='/about' component={About} />
+				<Route path='/git-hub' component={GitHub} />
+				<Route path='/contact' component={Contact} />
+				<Route path='/showcase' component={Showcase} />
 			</div>
 		</div>
-	);
+	)
 }
 
 export default App;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,31 +6,35 @@ const Landing = () => {
   
 
   return (
-    <div >
-      
+			<div>
+				<div className='design'>
+					<img src={coverPage} alt='graphic-design' className='coverPage' />
 
-   <div className='design'>
+					<div className='rotatedBorder'></div>
 
-     <img src={coverPage} alt='graphic-design' className='coverPage'/>
+					<div className='grid'>
+						<div data-white></div>
+						<div></div>
+						<div></div>
+						<div data-white></div>
+					</div>
 
-<div className='rotatedBorder'></div>
-
-<div className='grid'><div data-white></div><div></div><div></div><div data-white></div></div>
-
-<div className='blend'>
-    <img src={coverPage} alt='graphic-design' className='coverPage'/>
-    <div className='grid'><div></div><div data-white></div><div data-white></div><div></div></div>
-</div>
-
-      </div> 
-      <br/>
-      <section className='landWrapper'>
-      <h1>Kara R. Peoples</h1>
-      <h2>Full Stack Web Developer</h2>
-        <h3>Student</h3>
-      </section>
-    </div>
-  )
+					<div className='blend'>
+						<img src={coverPage} alt='tech' className='coverPage' />
+						<div className='grid'>
+							<div></div>
+							<div data-white></div>
+							<div data-white></div>
+							<div></div>
+						</div>
+					</div>
+				</div>
+				<br />
+				<section className='landWrapper'>
+					<h1 className='landInfo'>Kara R. Peoples</h1>
+				</section>
+			</div>
+		)
 }
 
 export default Landing;
