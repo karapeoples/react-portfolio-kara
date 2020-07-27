c998b64349105bcdf7766037c946ae5f15f92ea4
karapeoples <kararpeoples@yahoo.com>
2020-07-27 01:38:23 -0500

    Accoridon Fixed

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import avatar from '../images/avatar.jpg';
-
+import { Accordion, Icon } from 'semantic-ui-react'
 
 let oSkills = ['Fast Learner', 'Good Attitude', 'Friendly', 'Hard-Worker', 'Honest', "Creative"]
 let computer = ['Word', 'Excel', 'PowerPoint', 'VSCode Editor','Photoshop', 'InDesign', 'Illustrator', 'Bridge']
@@ -8,51 +8,64 @@ let language = ['HTML', 'CSS', 'LESS', 'JS', 'React', 'Redux']
 
 
 const About = () => {
+const [acc , setAcc]= useState({})
+const { activeIndex } = acc
+
+const handleClick = (e, titleProps) => {
+    const { index } = titleProps
+    const { activeIndex } = acc
+    const newIndex = activeIndex === index ? -1 : index
+
+    setAcc({ activeIndex: newIndex })
+  }
+
 
   return (
-    <section className='section'>
-      <span className='left-right'>
-				<div className='imageDiv'>
-          <img className='image' src={avatar} alt='Kara R. Peoples' />
-          <h1 class='aboutH1'>Kara R. Peoples</h1>
-				</div>
+			<section className='section'>
+				<span className='left-right'>
+					<div className='imageDiv'>
+						<img className='image' src={avatar} alt='Kara R. Peoples' />
+						<h1 class='aboutH1'>Kara R. Peoples</h1>
+					</div>
 
-				<section className='lists'>
-				<div class='ui styled accordion' animate children  >
-					<div class='active title' >
-						I am
-					</div>
-					<div class='active content'>
-						<ul>
-							{oSkills.map((info) => {
-								return <li> {info} </li>
-							})}
-						</ul>
-					</div>
-					<div class=' active title'>
-						A Full Stack Web Developer
-					</div>
-					<div class='active content'>
-						<ul>
-							{language.map((info) => {
-								return <li> {info} </li>
-							})}
-						</ul>
-					</div>
-					<div class='active title'>
-						Who Dabbles In
-					</div>
-					<div class='active content'>
-						<ul>
-							{computer.map((info) => {
-								return <li> {info} </li>
-							})}
-						</ul>
-					</div>
-          </div>
-        </section>
-        </span>
-        
+					<section className='lists'>
+						<Accordion styled>
+							<Accordion.Title class='active title' active={activeIndex === 0} index={0} onClick={handleClick}>
+								I am <Icon name='dropdown' />
+							</Accordion.Title>
+							<Accordion.Content active={activeIndex === 0}>
+								<ul>
+									{oSkills.map((info) => {
+										return <li> {info} </li>
+									})}
+								</ul>
+							</Accordion.Content>
+							<Accordion.Title active={activeIndex === 1} index={1} onClick={handleClick}>
+								A Full Stack Web Developer
+								<Icon name='dropdown' />
+							</Accordion.Title>
+							<Accordion.Content active={activeIndex === 1}>
+								<ul>
+									{language.map((info) => {
+										return <li> {info} </li>
+									})}
+								</ul>
+							</Accordion.Content>
+							<Accordion.Title active={activeIndex === 2} index={2} onClick={handleClick}>
+							Who Dabbles In
+							<Icon name='dropdown' />
+							</Accordion.Title>
+							<Accordion.Content active={activeIndex === 2}>
+								<ul>
+									{computer.map((info) => {
+										return <li> {info} </li>
+									})}
+								</ul>
+							</Accordion.Content>
+						</Accordion>
+					</section>
+				</span>
+
 				<p className='about-p'>
 					Being Disabled has taught me to appreciate the social experience that a job brings, while being thankful for being
 					able to be a productive member of society. It has also allowed me the opportunity to craft and hone my programming
